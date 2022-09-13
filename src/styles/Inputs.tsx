e079a41286803e1a6ef94f06f1f7974a4832d194
Micaías Silva <micaiasilvaop@gmail.com>
2022-09-13 13:45:42 -0300

    Feat: Base styles for Input

---
+++ src/styles/Inputs.tsx
@@ -0,0 +1,12 @@
+import { styled } from "./stitches.config";
+import { textBaseStyles } from "./Text";
+
+export const Input = styled("input", {
+  ...textBaseStyles,
+  display: "block",
+  border: "$grey3 2px solid",
+  background: "$grey3",
+  padding: "$1",
+  borderRadius: 10,
+  width: "100%",
+});
