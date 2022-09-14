49e789d4d33d1d3edb73adc7145ca48b81186226
Micaías Silva <micaiasilvaop@gmail.com>
2022-09-14 13:36:52 -0300

    Feat: Labels removed. added submit button

--- src/components/FormLogin/index.tsx
+++ src/components/FormLogin/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "../../styles/Buttons";
 import { Form } from "../../styles/Form";
 import { Input } from "../../styles/Inputs";
 import { Label } from "../../styles/Text";
@@ -5,10 +6,9 @@ import { Label } from "../../styles/Text";
 const FormLogin = () => {
   return (
     <Form>
-      <Label htmlFor="email">E-Mail</Label>
       <Input name="email" placeholder="Digite seu email" />
-      <Label htmlFor="password">Senha</Label>
       <Input name="password" placeholder="Digite sua senha" />
+      <Button shape={"fullSize"}>Logar</Button>
     </Form>
   );
 };
