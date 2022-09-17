2edaea5811a80c94071fb4792e60bdd735c383c8
Micaías Silva <micaiasilvaop@gmail.com>
2022-09-17 12:15:24 -0300

    Feat: Header inner padding removed

--- src/components/Header/style.tsx
+++ src/components/Header/style.tsx
@@ -2,8 +2,10 @@ import { styled } from "../../styles/stitches.config";
 
 export const HeaderStyle = styled("header", {
   minHeight: "3rem",
-  padding: "0 $1",
   display: "flex",
   alignItems: "center",
   justifyContent: "space-between",
+  // position: "fixed",
+  // width: "100%",
+  // background: "$grey1",
 });
