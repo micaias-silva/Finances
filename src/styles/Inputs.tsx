00e5dc0563d721268012df5cadb7538e66ec167f
Micaías Silva <micaiasilvaop@gmail.com>
2022-09-17 12:14:18 -0300

    Feat: Styles for <select>

--- src/styles/Inputs.tsx
+++ src/styles/Inputs.tsx
@@ -9,4 +9,15 @@ export const Input = styled("input", {
   padding: "$1",
   borderRadius: 10,
   width: "100%",
+  "&:focus": {},
+});
+
+export const Select = styled("select", {
+  ...textBaseStyles,
+  display: "block",
+  border: "$grey3 2px solid",
+  background: "$grey3",
+  padding: "$1",
+  borderRadius: 10,
+  width: "100%",
 });
